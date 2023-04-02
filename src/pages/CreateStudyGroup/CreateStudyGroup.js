8ed57d615add754b8fb95ce01f4795c28b91c180
yi-ting <yumy19990628@gmail.com>
2023-04-02 22:24:43 +0800

    [Add] development environment

---
+++ src/pages/CreateStudyGroup/CreateStudyGroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import styled from 'styled-components/macro'
+ 
+function CreateStudyGroup(){
+    return (<>
+        <input/>
+        <input/>
+        <input/>
+    </>)
+}
+export default CreateStudyGroup
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link to={`./create`}>創建讀書會</Link>
+      </li>
+      <li>
+        <Link to={`./studyGroups`}>所有讀書會</Link>
+      </li>
+      <li>
+        <Link to={`./member`}>會員</Link>
+      </li>
+    </ul>
+  );
+}
+export default Home;
