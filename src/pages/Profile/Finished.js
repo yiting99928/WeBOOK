96784312b10ed879657a4f1bc4a4f92e4d66a7d4
yi-ting <yumy19990628@gmail.com>
2023-04-06 00:31:38 +0800

    [Add] user finiehed studyGroupData

--- src/pages/Profile/Finished.js
+++ src/pages/Profile/Finished.js
@@ -1,6 +1,76 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import SideMenu from '../../components/SideMenu';
+import { db } from '../../utils/firebase';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 
+const Finished = () => {
+  const [groupData, setGroupData] = useState([]);
+  useEffect(() => {
+    async function loadGroupData() {
+      try {
+        const userRef = doc(db, 'users', 'yumy19990628@gmail.com');
+        const userStudyGroupsRef = collection(userRef, 'UserStudyGroups');
+        const userStudyGroupsSnapshot = await getDocs(userStudyGroupsRef);
+
+        const allGroupsData = [];
+
+        for (const groupDoc of userStudyGroupsSnapshot.docs) {
+          const groupId = groupDoc.id;
+          const groupNote = groupDoc.data().note;
+          const studyGroupRef = doc(db, 'studyGroups', groupId);
+          const studyGroupDoc = await getDoc(studyGroupRef);
+
+          allGroupsData.push({
+            id: groupId,
+            note: groupNote,
+            ...studyGroupDoc.data(),
+          });
+        }
+        setGroupData(allGroupsData);
+      } catch (error) {
+        console.error('Error: ', error);
+      }
+    }
+
+    loadGroupData();
+  }, []);
+
+  return (
+    <Container>
+      <SideMenu isOpen={true} />
+      <Content isOpen={true}>
+        {groupData.length === 0 ? (
+          <p>目前沒有結束的讀書會</p>
+        ) : (
+          groupData.map((item, i) => (
+            <StudyGroupCard key={i}>
+              <BookImg imageUrl={item.image} />
+              <CardContent>
+                <p>
+                書名:<span>{item.name}</span>
+                </p>
+                <p>
+                  作者:<span>{item.author}</span>
+                </p>
+                <p>
+                  導讀者:<span>{item.createBy}</span>
+                </p>
+                <p>
+                  章節:<span>{item.chapter}</span>
+                </p>
+                <p>
+                  舉辦時間:<span>{item.hold}</span>
+                </p>
+                <p>筆記：{item.note}</p>
+              </CardContent>
+            </StudyGroupCard>
+          ))
+        )}
+      </Content>
+    </Container>
+  );
+};
 const Container = styled.div`
   display: flex;
   min-height: 100vh;
@@ -13,14 +83,20 @@ const Content = styled.div`
   width: ${(props) => (props.isOpen ? 'calc(100% - 200px)' : '100%')};
 `;
 
-function Finished() {
-  return (
-    <Container>
-      <SideMenu />
-      <Content>
-        <p>結束</p>
-      </Content>
-    </Container>
-  );
-}
+const StudyGroupCard = styled.div`
+  display: flex;
+  border: 1px solid black;
+  line-height: 15px;
+`;
+const BookImg = styled.div`
+  background-image: url(${(props) => props.imageUrl});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 250px;
+  height: 300px;
+  margin-right: 30px;
+`;
+const CardContent = styled.div`
+  width: 800px;
+`;
 export default Finished;
