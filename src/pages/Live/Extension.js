15d143e20967af0cae5505c10042931eabbf4c53
yi-ting <yumy19990628@gmail.com>
2023-04-09 21:45:09 +0800

    [Remove] redundant file

--- src/pages/Live/Extension.js
+++
@@ -1,9 +0,0 @@
-function Extension() {
-    return (
-      <div>
-        <p>這是導讀講稿的文字內容</p>
-      </div>
-    );
-  }
-  export default Extension;
-  
--- src/pages/Live/StickyNote.js
+++
@@ -1,8 +0,0 @@
-function StickyNote() {
-  return (
-    <div>
-      <p>這是導讀講稿的文字內容</p>
-    </div>
-  );
-}
-export default StickyNote;
