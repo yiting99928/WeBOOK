be3c3ad60a6b4379a5d614659d454386a63de13b
yi-ting <yumy19990628@gmail.com>
2023-04-16 20:39:51 +0800

    [Update] homePage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,9 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../context/authContext';
+// import React, { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+// import { AuthContext } from '../../context/authContext';
 
 function Home() {
-  const { user } = useContext(AuthContext);
-  return (
-    <ul>
-      <li>
-        <Link to={user ? '/create' : '/login'}>創建讀書會</Link>
-      </li>
-      <li>
-        <Link to={`/studyGroups`}>所有讀書會</Link>
-      </li>
-      <li>
-        <Link to={user ? '/profile' : '/login'}>會員</Link>
-      </li>
-    </ul>
-  );
+  // const { user } = useContext(AuthContext);
+  return <div>這裡之後會隨機出現一些讀書會</div>;
 }
 export default Home;
