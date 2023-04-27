1fa6fcfde278abc308646cc82e7b5dd145535bef
yi-ting <yumy19990628@gmail.com>
2023-04-27 17:44:26 +0800

    [Add] outline button component

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -7,6 +7,13 @@ const MainBtn = ({ children, onClick, height = '36px' }) => {
     </MainBtnStyled>
   );
 };
+const OutlineBtn = ({ children, onClick, isHost }) => {
+  return (
+    <OutlineBtnStyled onClick={onClick} isHost={isHost}>
+      {children}
+    </OutlineBtnStyled>
+  );
+};
 
 const MainBtnStyled = styled.div`
   cursor: pointer;
@@ -22,6 +29,32 @@ const MainBtnStyled = styled.div`
   font-size: 16px;
   margin-top: 8px;
 `;
+const OutlineBtnStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 8px 4px 8px;
+  border: 2px solid #ffac4c;
+  border-radius: 5px;
+  background-color: white;
+  color: #ffac4c;
+  cursor: pointer;
+  letter-spacing: 1.2;
+  font-size: 14px;
+  :hover {
+    background-color: #ffac4c;
+    color: #fff;
+  }
+`;
+const GuestEditInput = styled(OutlineBtnStyled)`
+  display: ${({ isHost }) => (isHost ? 'none' : 'inline-block')};
+`;
+const HostEditInput = styled(OutlineBtnStyled)`
+  display: ${({ isHost }) => (isHost ? 'inline-block' : 'none')};
+`;
 
 // export default Buttons;
 export { MainBtn };
+export { OutlineBtn };
+export { GuestEditInput };
+export { HostEditInput };
