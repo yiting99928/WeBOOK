2cac32d2cd23b209dccbe2d7fbb3782a50e6af4a
yi-ting <yumy19990628@gmail.com>
2023-04-27 17:30:56 +0800

    [Add] modal utils

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -2,8 +2,10 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import webookJump from './webookJump.gif';
 import webookSwing from './webookSwing.gif';
+// import { useNavigate } from 'react-router-dom';
 
 const modal = {
+  // const navigate = useNavigate();
   success(text) {
     const success = withReactContent(Swal);
     success.fire({
@@ -14,6 +16,22 @@ const modal = {
       scrollbarPadding: false,
     });
   },
+  create(text, id) {
+    const success = withReactContent(Swal);
+    success
+      .fire({
+        title: text,
+        imageUrl: webookJump,
+        imageWidth: 100,
+        imageAlt: 'success',
+        scrollbarPadding: false,
+      })
+      .then((result) => {
+        if (result.isConfirmed && id) {
+          window.location = `/studyGroup/${id}`;
+        }
+      });
+  },
   fail(text) {
     const fail = withReactContent(Swal);
     fail.fire({
@@ -23,6 +41,32 @@ const modal = {
       imageAlt: 'fail',
       scrollbarPadding: false,
     });
+  },
+  quit(text) {
+    const fail = withReactContent(Swal);
+    fail.fire({
+      title: text,
+      imageUrl: webookSwing,
+      imageWidth: 100,
+      imageAlt: 'quit',
+      scrollbarPadding: false,
+    });
+  },
+  noUser(text) {
+    const fail = withReactContent(Swal);
+    fail
+      .fire({
+        title: text,
+        imageUrl: webookSwing,
+        imageWidth: 100,
+        imageAlt: 'quit',
+        scrollbarPadding: false,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          window.location = `/login`;
+        }
+      });
   },
 };
 export default modal;
