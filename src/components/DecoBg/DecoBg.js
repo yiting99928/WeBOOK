71f7f11c2c5d77d6614cdfecfa156faf78a07e8d
yi-ting <yumy19990628@gmail.com>
2023-04-29 20:18:27 +0800

    [Add] background transform

--- src/components/DecoBg/DecoBg.js
+++ src/components/DecoBg/DecoBg.js
@@ -1,50 +1,107 @@
-import styled from 'styled-components/macro';
-function DecoBg({ height = 600 }) {
+import styled, { keyframes } from 'styled-components/macro';
+
+function DecoBg() {
   return (
-    <Decos height={height}>
-      <Deco />
-      <Deco2 />
-      <Deco3 />
-      <Deco4 />
-    </Decos>
+      <Decos>
+        <Deco />
+        <Deco2 />
+        <Deco3 />
+        <Deco4 />
+      </Decos>
   );
 }
 
+const move1 = keyframes`
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(75%, 0%);
+  }
+`;
+
+const move2 = keyframes`
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(100%, 20%);
+  }
+`;
+
+const move3 = keyframes`
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(80%, 0%);
+  }
+`;
+
+const move4 = keyframes`
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(-100%, 0%);
+  }
+`;
+
+
 const Decos = styled.div`
   position: absolute;
-  display: flex;
-  width: 100%;
-  height: ${({ height }) => `${height}px`};
-  align-items: center;
-  justify-content: space-between;
   z-index: -1;
+  width: 100%;
+  height: 100vh;
+  display: flex;
 `;
+
 const Deco = styled.div`
-  width: 50%;
-  height: 50%;
-  background: rgba(239, 140, 138, 0.3);
+  position: absolute;
+  width: 40%;
+  height: 90%;
+  background: rgba(239, 140, 138, 0.25);
+  top: -20%;
+  left: -10%;
   filter: blur(100px);
-  margin-top: -10%;
-  margin-left: -30%;
+  border-radius: 50%;
+  animation: ${move1} 5s linear infinite alternate;
 `;
+
 const Deco2 = styled.div`
-  width: 20%;
-  height: 20%;
-  background: rgba(231, 93, 16, 0.3);
-  filter: blur(80px);
-`;
-const Deco3 = styled.div`
+  left: 30%;
+  position: absolute;
   width: 25%;
-  height: 25%;
-  background: rgba(96, 160, 255, 0.3);
-  align-self: flex-end;
-  filter: blur(80px);
-  margin-bottom: 50px;
+  height: 60%;
+  top: 20%;
+  background: rgba(231, 93, 16, 0.2);
+  filter: blur(100px);
+  border-radius: 50%;
+  animation: ${move2} 3s linear infinite alternate;
 `;
+
+const Deco3 = styled.div`
+  position: absolute;
+  width: 20%;
+  height: 45%;
+  right: 20%;
+  bottom: 0px;
+  background: rgba(96, 160, 255, 0.2);
+  border-radius: 50%;
+  filter: blur(40px);
+  animation: ${move3} 5s linear infinite alternate;
+`;
+
 const Deco4 = styled.div`
-  width: 10%;
-  height: 35%;
+  position: absolute;
+  width: 15%;
+  height: 30%;
+  right: 0px;
+  top: 25%;
   background: rgba(255, 172, 76, 0.3);
-  filter: blur(80px);
+  border-radius: 50%;
+  filter: blur(50px);
+  animation: ${move4} 2s linear infinite alternate;
 `;
+
 export default DecoBg;
