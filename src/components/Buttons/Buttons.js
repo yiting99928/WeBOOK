c62b6e4d721e5a5a80457f14b120d29bbd3fc3ed
yi-ting <yumy19990628@gmail.com>
2023-05-01 00:16:24 +0800

    [Add] main button hover

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -21,13 +21,16 @@ const MainBtnStyled = styled.div`
   justify-content: center;
   align-items: center;
   height: ${({ height }) => height};
-  background: #ffac4c;
+  background-color: #ffac4c;
   border-radius: 10px;
   color: white;
   font-weight: 600;
   letter-spacing: 1.2;
   font-size: 16px;
   margin-top: 8px;
+  :hover {
+    background-color: rgb(223, 82, 77);
+  }
 `;
 const OutlineBtnStyled = styled.div`
   display: flex;
