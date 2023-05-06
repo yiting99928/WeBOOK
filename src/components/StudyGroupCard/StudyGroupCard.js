6233f91ed347c2e5c44e4fd898cfbf9379a3a108
yi-ting <yumy19990628@gmail.com>
2023-05-06 23:31:02 +0800

    [Remove] btn

--- src/components/StudyGroupCard/StudyGroupCard.js
+++ src/components/StudyGroupCard/StudyGroupCard.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components/macro';
 import moment from 'moment';
-import { MainBtn } from '../Buttons/Buttons';
 import { useNavigate } from 'react-router-dom';
+import { BiTimeFive } from 'react-icons/bi';
 
-function StudyGroupCard({ item, onClick, onJoinGroup }) {
+function StudyGroupCard({ item }) {
   const navigate = useNavigate();
   return (
-    <BookGroup onClick={onClick}>
+    <BookGroup onClick={() => navigate(`/studyGroup/${item.id}`)}>
       <ImgContainer>
-        <BookGroupImg
-          src={item.image}
-          alt="feature"
-          onClick={() => navigate(`/studyGroup/${item.id}`)}
-        />
+        {item.status === 'ongoing' ? (
+          <LiveTag>
+            <LiveIcon />
+            直播中
+          </LiveTag>
+        ) : (
+          <div></div>
+        )}
+        <BookGroupImg src={item.image} alt="feature" />
       </ImgContainer>
       <BookGroupInfo>
         <BookTitle>{item.groupName}</BookTitle>
@@ -21,23 +25,40 @@ function StudyGroupCard({ item, onClick, onJoinGroup }) {
         </p>
         <Creator>
           導讀人：{item.host}
-          <br />
-          時間：
-          {moment.unix(item.startTime.seconds).format('MM.DD HH:mm')}
+          <div>
+            <BiTimeFive />
+            {moment.unix(item.startTime.seconds).format('MM.DD HH:mm')}
+          </div>
         </Creator>
-        <MainBtn
-          onClick={(e) => {
-            onJoinGroup(item.id);
-          }}>
-          加入讀書會
-        </MainBtn>
       </BookGroupInfo>
     </BookGroup>
   );
 }
+const LiveTag = styled.div`
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  background-color: #df524d;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  border-radius: 25px;
+  color: #fff;
+  gap: 5px;
+  z-index: 1;
+  font-size: 14px;
+`;
+const LiveIcon = styled.div`
+  width: 12px;
+  height: 12px;
+  background-image: url(//cdn-cos-ke.myoed.com/ke_proj/core/_next/static/media/live-icon-1.0b738b5c.gif);
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
 const ImgContainer = styled.div`
   overflow: hidden;
   height: 100%;
+  position: relative;
 `;
 const BookTitle = styled.div`
   padding-bottom: 4px;
@@ -50,18 +71,29 @@ const Creator = styled.div`
   font-size: 14px;
   margin-top: auto;
   line-height: 1.5;
+  div {
+    background-color: #ffac4c;
+    border-radius: 25px;
+    padding-left: 10px;
+    color: #fff;
+    padding-top: 2px;
+    width: 115px;
+    margin-top: 5px;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
 `;
 
 const BookGroupImg = styled.img`
   height: 100%;
   object-fit: cover;
-  cursor: pointer;
   :hover {
-    transform: scale(1.2);
+    transform: scale(1.15);
   }
 `;
 const BookGroupInfo = styled.div`
-  height: 250px;
+  height: 180px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -78,6 +110,7 @@ const BookGroup = styled.div`
   border: 1px solid #ececec;
   box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.1);
   overflow: hidden;
-  max-height: 510px;
+  max-height: 480px;
+  cursor: pointer;
 `;
 export default StudyGroupCard;
